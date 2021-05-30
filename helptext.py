c05527e010ba0110cabf061a5c97697ddb88d013
Jon Yoder <jon@yoder.cloud>
2021-05-30 15:12:39 -0400

    Formatting and grammatical revisions

--- helptext.py
+++ helptext.py
@@ -1,12 +1,14 @@
 '''This module merely stores the extensive help text for different commands to 
 ensure the code remains easy to read.'''
 
-login_cmd = '''Usage: login <address>
-Log into a server once connected. The address used may be the numeric address
-(e.g. 557207fd-0a0a-45bb-a402-c38461251f8f) or the friendly address (e.g. 
-CatLover). Because multiple domains could be hosted by the same server, it is 
-necessary to give the entire workspace address. Note that if the address is 
-omitted, the command is assumed to log into main account for the profile.
+login_cmd = '''Usage: login [address]
+
+Log into a server once connected. The address used may be the regular Mensago
+address (catlover/example.com) or the workspace address
+(e.g. 557207fd-0a0a-45bb-a402-c38461251f8f). Because multiple domains could be
+hosted by the same server, it is necessary to give the entire workspace
+address. Note that if the address is omitted, the command is assumed to log
+into main account for the profile.
 
 Examples:
 login f009338a-ea14-4d59-aa48-016829835cd7/example.com
@@ -15,11 +17,12 @@ login
 '''
 
 preregister_cmd='''Usage: preregister <port_number> [user_id]
+
 Preprovisions a workspace for a user. This command only works when logged in 
-as the administrator. A user ID may be passed to the command, but this is 
-optional. The command returns a workspace ID and a preregistration code. The 
-user will perform the initial login with either the workspace ID or the user 
-ID and the registration code.
+as the administrator. An optional user ID may be passed to the command. The
+command returns a workspace ID and a preregistration code. The user will
+perform the initial login with either the workspace ID or the user ID and the
+registration code.
 '''
 
 profile_cmd = '''Usage: profile <action> <profilename>
@@ -44,7 +47,8 @@ profile exists, this action has no effect.
 
 set <name> - activates the specified profile and deactivates the current one.'''
 
-register_cmd = '''Usage: register <serveraddress>
+register_cmd = '''Usage: register <server>
+
 Register a new workspace account. This command requires a connection to a
 server. Depending on the registration type set on the server, this command may
 return a status other than success or failure. If a server immediately creates
@@ -52,17 +56,20 @@ a new workspace account, this command will print the new numeric address
 created.'''
 
 shell_cmd = '''Usage: shell <command>
+
 Executes a command directly in the regular user shell. On Windows, this is 
 Command Prompt. On UNIX-like platforms, this is the default shell, usually
 bash.
 Aliases: ` , sh'''
 
 setinfo_cmd = '''Usage: setinfo <infotype> <value>
+
 Sets contact information for the profile. Available information which can be 
 set is listed below:
 '''
 
 setuserid_cmd = '''Usage: setuserid <userid>
+
 Sets the user ID for the profile. This is the part before the slash in your 
 Mensago address.
 
