3064160ea3d2a676238d957f56f94b74a8132b49
Jon Yoder <jon@yoder.cloud>
2022-02-19 09:52:03 -0500

    Update to have better tests

--- utils/argonbench/src/main.rs
+++ utils/argonbench/src/main.rs
@@ -2,25 +2,44 @@ use argon2::{self, Config, ThreadMode, Variant, Version};
 use rand::Rng;
 use stopwatch::{Stopwatch};
 
-fn hash_password(password: &str) {
+fn hashbench() {
+	let password = "MyS3cretPassw*rd";
 	let salt = rand::thread_rng().gen::<[u8; 16]>();
+
+	let testlist = [
+		(0x10000, 1, 2), // 64K
+		(0x100_000, 1, 2), // 1MiB
+		(0x200_000, 1, 2), // 2MiB
+
+		(0x10000, 1, 4), // 64K
+		(0x100_000, 1, 4), // 1MiB
+		(0x200_000, 1, 4), // 2MiB
+
+		(0x10000, 2, 2), // 64K
+		(0x100_000, 2, 2), // 1MiB
+		(0x200_000, 2, 2), // 2MiB
+	];
 	
-	let config = Config {
-		variant: Variant::Argon2id,
-		version: Version::Version13,
-		mem_cost: 65536,
-		time_cost: 1,
-		lanes: 2,
-		thread_mode: ThreadMode::Parallel,
-		secret: &[],
-		ad: &[],
-		hash_length: 32
-	};
-	let sw = Stopwatch::start_new();
-	let hash = argon2::hash_encoded(password.as_bytes(), &salt, &config).unwrap();
-	println!("The hash is {} and took {}ms to generate", hash, sw.elapsed_ms());
+	for test in testlist {
+		let config = Config {
+			variant: Variant::Argon2id,
+			version: Version::Version13,
+			mem_cost: test.0, // 64K
+			time_cost: test.1,
+			lanes: test.2,
+			thread_mode: ThreadMode::Parallel,
+			secret: &[],
+			ad: &[],
+			hash_length: 32
+		};
+		let sw = Stopwatch::start_new();
+		argon2::hash_encoded(password.as_bytes(), &salt, &config).unwrap();
+		let elapsed = sw.elapsed_ms();
+
+		println!("Test: M:{} T: {}, P:{}\t {}ms", test.0, test.1, test.2, elapsed);
+	}
 }
 
 fn main() {
-	hash_password("MySecretPassword");
+	hashbench();
 }
